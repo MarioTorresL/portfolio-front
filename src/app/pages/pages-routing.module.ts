9e635b47b37bac247d2934f19ce1c063625d916f
Mario Torres <mario.torreslepe@gmail.com>
2022-09-29 21:18:58 -0300

    add technologies images in view

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,9 +2,15 @@ import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import {PagesComponent} from './pages.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes =[
-  {path: 'home', component: PagesComponent}
+  {
+    path: 'home', component: PagesComponent,
+    children:[
+      {path:'', component: HomeComponent}
+    ]
+  }
 ]
 
 
