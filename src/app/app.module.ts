5df13c993e3888c4b36a2bf7c60bec5ab1914dba
Mario Torres <mario.torreslepe@gmail.com>
2022-10-22 19:20:08 -0300

    fix import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,7 @@ import {PagesModule} from './pages/pages.module';
     PagesModule,
     AuthModule
   ],
-  providers: [],
+  providers: [ {provide: APP_BASE_HREF, useValue: '/portfolio-front'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
