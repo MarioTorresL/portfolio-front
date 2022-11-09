d575c06da36f8aabc61b9a708557bec55797807b
Mario Torres <mario.torreslepe@gmail.com>
2022-11-08 21:52:20 -0300

    add crud in my comments section

--- src/app/pages/my-comment/my-comment.component.ts
+++ src/app/pages/my-comment/my-comment.component.ts
@@ -1,17 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+import { CommentService } from 'src/app/services/comment.service';
 import {AuthService} from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-my-comment',
   templateUrl: './my-comment.component.html',
-  styleUrls: ['./my-comment.component.css']
+  styleUrls: ['./my-comment.component.css'],
 })
 export class MyCommentComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  public comments!:any
+  public uid: number = 0
+  public edit:boolean = false
+
+  constructor(private commentsService: CommentService, private authService: AuthService) {}
 
   ngOnInit(): void {
-    console.log(this.authService.user)
+    this.uid = this.authService.uid;
+    this.myComments()
   }
 
+  myComments(){
+    this.commentsService.getMyComments(this.uid).subscribe(resp=>{
+      this.comments = resp.data
+    }) 
+  }
+
+  new(comment: string) {
+    this.commentsService.post( comment ).subscribe((resp) => {
+      this.myComments()
+      Swal.fire({
+        heightAuto: false,
+        icon: 'success',
+        title: 'Your comment has been posted',
+        showConfirmButton: false,
+        timer: 500,
+      });
+    });
+  }
+
+  update(id:number, comment:string){
+    this.edit = true
+    return this.commentsService.put(id, comment).subscribe(resp=>{
+      console.log(resp)
+      this.edit=false
+      this.myComments()
+      Swal.fire({
+        heightAuto: false,
+        icon: 'success',
+        title: 'Your comment has been Deleted',
+        showConfirmButton: false,
+        timer: 500,
+      });
+    })
+  }
+
+  delete(uid:number){
+    return this.commentsService.delete(uid).subscribe(resp=>{
+      this.myComments()
+      Swal.fire({
+        heightAuto: false,
+        icon: 'success',
+        title: 'Your comment has been Deleted',
+        showConfirmButton: false,
+        timer: 500,
+      });
+    })
+  }
 }
--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {environment} from 'src/environments/environment';
+import {Response} from '../interfaces/response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -28,11 +29,19 @@ export class CommentService {
     return this.http.get<any>(`${this.API}/comments`)
   }
 
-  post(formComment:any){
-    return this.http.post(`${this.API}/comments`, formComment, this.headers )
+  post(comment:string){
+    return this.http.post(`${this.API}/comments`, {comment}, this.headers )
   }
-  //put(comment:any){
-  //  console.log(comment)
-  //  return this.http.put(`${this.API}/comments`, comment, this.headers)
-  //}
+
+  getMyComments(uid:number){
+    return this.http.get<Response>(`${this.API}/comments/${uid}`, this.headers)
+  }
+
+  put(uid:number, comment:string){
+    return this.http.put(`${this.API}/comments/${uid}`, {comment}, this.headers)
+  }
+
+  delete(uid:number){
+    return this.http.delete(`${this.API}/comments/${uid}`, this.headers)
+  }
 }
